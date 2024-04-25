4c55aed389a951a24d5b5e73a9db3028d893a280
German Terrile <gterrile@gmail.com>
2024-04-24 23:32:08 -0700

    fixed images load

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,11 @@
+import dragon from './assets/characters/dragon.png'
 
 function Card({character, handleClick}) {
   return (
     
     <div className="card-container">
       <div className="card">
-        <img src="src/assets/characters/dragon.png" className="card-back" alt="dragonLogo" />
+        <img src= {dragon} className="card-back" alt="dragonLogo" />
         <img src={character.img} alt={character.name} className='card-front' id={character.id} onClick={(e)=> handleClick(e)}/>
       </div>
     </div>
--- src/Helper.jsx
+++ src/Helper.jsx
@@ -1,25 +1,43 @@
 //
 // Load and export list of characters
+import bulma from './assets/characters/bulma.png'
+import majinbu from './assets/characters/majinbu2.png'
+import freezer from './assets/characters/freezer.png'
+import goku from './assets/characters/goku2.png'
+import gohan from './assets/characters/gohan.png'
+import krillin from './assets/characters/krillin.png'
+import piccolo from './assets/characters/piccolo.png'
+import roshi from './assets/characters/roshi.png'
+import satan from './assets/characters/satan.png'
+import trunks from './assets/characters/trunks.png'
+import vegeta from './assets/characters/vegeta.png'
+import androide from './assets/characters/androide.png'
+import goten from './assets/characters/goten.png'
+import oldkai from './assets/characters/oldkai.png'
+import tenjinhan from './assets/characters/tenjinhan.png'
+import chichi from './assets/characters/chichi.png'
+import yayirobe from './assets/characters/yayirobe.png'
+import pilaf from './assets/characters/pilaf.png'
 
 export const cards = [
-  {id: crypto.randomUUID(), name: 'Bulma', img: '/src/assets/characters/bulma.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'Majinbu', img: '/src/assets/characters/majinbu2.png', clicked: false}, 
-  {id: crypto.randomUUID(), name: 'Freezer', img: '/src/assets/characters/freezer.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'Gohan', img: '/src/assets/characters/gohan.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'Goku', img: '/src/assets/characters/goku2.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'Krillin', img: '/src/assets/characters/krillin.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'Piccolo', img: '/src/assets/characters/piccolo.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'Roshi', img: '/src/assets/characters/roshi.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'Satan', img: '/src/assets/characters/satan.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'Trunks', img: '/src/assets/characters/trunks.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'Vegeta', img: '/src/assets/characters/vegeta.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'Androide', img: '/src/assets/characters/androide.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'Goten', img: '/src/assets/characters/goten.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'OldKai', img: '/src/assets/characters/oldkai.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'Tenjinhan', img: '/src/assets/characters/tenjinhan.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'Chichi', img: '/src/assets/characters/chichi.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'Yayirobe', img: '/src/assets/characters/yayirobe.png', clicked: false},
-  {id: crypto.randomUUID(), name: 'Pilaf', img: '/src/assets/characters/pilaf.png', clicked: false}
+  {id: crypto.randomUUID(), name: 'Bulma', img: bulma, clicked: false},
+  {id: crypto.randomUUID(), name: 'Majinbu', img: majinbu, clicked: false}, 
+  {id: crypto.randomUUID(), name: 'Freezer', img: freezer, clicked: false},
+  {id: crypto.randomUUID(), name: 'Gohan', img: gohan, clicked: false},
+  {id: crypto.randomUUID(), name: 'Goku', img: goku, clicked: false},
+  {id: crypto.randomUUID(), name: 'Krillin', img: krillin, clicked: false},
+  {id: crypto.randomUUID(), name: 'Piccolo', img: piccolo, clicked: false},
+  {id: crypto.randomUUID(), name: 'Roshi', img: roshi, clicked: false},
+  {id: crypto.randomUUID(), name: 'Satan', img: satan, clicked: false},
+  {id: crypto.randomUUID(), name: 'Trunks', img: trunks, clicked: false},
+  {id: crypto.randomUUID(), name: 'Vegeta', img: vegeta, clicked: false},
+  {id: crypto.randomUUID(), name: 'Androide', img: androide, clicked: false},
+  {id: crypto.randomUUID(), name: 'Goten', img: goten, clicked: false},
+  {id: crypto.randomUUID(), name: 'OldKai', img: oldkai, clicked: false},
+  {id: crypto.randomUUID(), name: 'Tenjinhan', img: tenjinhan, clicked: false},
+  {id: crypto.randomUUID(), name: 'Chichi', img: chichi, clicked: false},
+  {id: crypto.randomUUID(), name: 'Yayirobe', img: yayirobe, clicked: false},
+  {id: crypto.randomUUID(), name: 'Pilaf', img: pilaf, clicked: false}
 ]
 
 //
